5a9ac60d13a592aa8b6b8e329fc235cccbeeed66
ramirezfernando <ramirez.fernando2003@gmail.com>
2024-03-27 16:40:17 -0500

    refactor: go fmt main.go

--- src/main.go
+++ src/main.go
@@ -12,51 +12,52 @@ import (
 type clocMap = map[string]int
 
 func main() {
-    if len(os.Args) < 2 {
-        fmt.Println("Usage: cloc <folder>")
-        os.Exit(1)
-    }
-    folderPath := os.Args[1]
-    lines, err := countLinesOfCode(folderPath)
-    if err != nil {
-        fmt.Printf("Error: %v\n", err)
-        os.Exit(1)
-    }
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: cloc <folder>")
+		os.Exit(1)
+	}
+	folderPath := os.Args[1]
+	lines, err := countLinesOfCode(folderPath)
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
+	}
 
-    for lang, li := range lines {
-        fmt.Printf("Total lines of code for %q: %d\n", lang, li)
-    }
+	for lang, li := range lines {
+		fmt.Printf("Total lines of code for %q: %d\n", lang, li)
+	}
 }
 
 func countLinesOfCode(folderPath string) (clocMap, error) {
-    cloc := make(clocMap)
-    err := filepath.WalkDir(folderPath, func(path string, info fs.DirEntry, err error) error {
-        if err != nil {
-            return err
-        }
-        if !info.IsDir() && info.Type().IsRegular() {
-            val, exists := language.Exts[filepath.Ext(path)]; if exists {
-                lines, err := countLinesOfFile(path)
-                if err != nil {
-                    return err
-                }
-                cloc[val] += lines
-            }
-        }
+	cloc := make(clocMap)
+	err := filepath.WalkDir(folderPath, func(path string, info fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() && info.Type().IsRegular() {
+			val, exists := language.Exts[filepath.Ext(path)]
+			if exists {
+				lines, err := countLinesOfFile(path)
+				if err != nil {
+					return err
+				}
+				cloc[val] += lines
+			}
+		}
 
-        return nil
-    })
-    if err != nil {
-        return cloc, err
-    }
-    return cloc, nil
+		return nil
+	})
+	if err != nil {
+		return cloc, err
+	}
+	return cloc, nil
 }
 
 func countLinesOfFile(filename string) (int, error) {
-    content, err := os.ReadFile(filename)
-    if err != nil {
-        return 0, err
-    }
-    lines := strings.Split(string(content), "\n")
-    return len(lines), nil
+	content, err := os.ReadFile(filename)
+	if err != nil {
+		return 0, err
+	}
+	lines := strings.Split(string(content), "\n")
+	return len(lines), nil
 }
