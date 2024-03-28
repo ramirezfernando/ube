856df93d3a999f8d6d8065a699dde981d8f518b5
ramirezfernando <ramirez.fernando2003@gmail.com>
2024-03-28 15:42:06 -0500

    refactor: create generate table function

--- src/main.go
+++ src/main.go
@@ -28,45 +28,12 @@ func main() {
 	folderPath := os.Args[1]
 
 	lines, err := countLinesOfCode(folderPath)
-
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
 		os.Exit(1)
 	}
 
-	columns := []table.Column{
-		{Title: "Language", Width: 15},
-		{Title: "Lines of Code", Width: 15},
-	}
-
-	rows := []table.Row{}
-	for lang, li := range lines {
-		rows = append(rows, table.Row{lang, strconv.Itoa(li)})
-	}
-	sort.Slice(rows, func(i, j int) bool {
-		li1, _ := strconv.Atoi(rows[i][1])
-		li2, _ := strconv.Atoi(rows[j][1])
-		return li1 > li2
-	})
-
-	t := table.New(
-		table.WithColumns(columns),
-		table.WithRows(rows),
-		table.WithFocused(true),
-		table.WithHeight(7),
-	)
-
-	s := table.DefaultStyles()
-	s.Header = s.Header.
-		BorderStyle(lipgloss.NormalBorder()).
-		BorderForeground(lipgloss.Color("240")).
-		BorderBottom(true).
-		Bold(false)
-	s.Selected = s.Selected.
-		Foreground(lipgloss.Color("229")).
-		Background(lipgloss.Color("#5433ff")).
-		Bold(false)
-	t.SetStyles(s)
+    t := generateTable(lines)
 
 	m := terminal.Model{Table: t, Help: help.New()}
 	if _, err := tea.NewProgram(m).Run(); err != nil {
@@ -108,4 +75,42 @@ func countLinesOfFile(filename string) (int, error) {
 	}
 	lines := strings.Split(string(content), "\n")
 	return len(lines), nil
+}
+
+func generateTable(lines clocMap) table.Model {
+    columns := []table.Column{
+		{Title: "Language", Width: 15},
+		{Title: "Lines of Code", Width: 15},
+	}
+
+	rows := []table.Row{}
+	for lang, li := range lines {
+		rows = append(rows, table.Row{lang, strconv.Itoa(li)})
+	}
+	sort.Slice(rows, func(i, j int) bool {
+		li1, _ := strconv.Atoi(rows[i][1])
+		li2, _ := strconv.Atoi(rows[j][1])
+		return li1 > li2
+	})
+
+	t := table.New(
+		table.WithColumns(columns),
+		table.WithRows(rows),
+		table.WithFocused(true),
+		table.WithHeight(7),
+	)
+
+	s := table.DefaultStyles()
+	s.Header = s.Header.
+		BorderStyle(lipgloss.NormalBorder()).
+		BorderForeground(lipgloss.Color("240")).
+		BorderBottom(true).
+		Bold(false)
+	s.Selected = s.Selected.
+		Foreground(lipgloss.Color("229")).
+		Background(lipgloss.Color("#5433ff")).
+		Bold(false)
+	t.SetStyles(s)
+
+    return t
 }
